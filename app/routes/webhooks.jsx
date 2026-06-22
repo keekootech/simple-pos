36ecc4250b02cad5bb5e584e05f06d7dcdec537e
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-06-23 00:11:53 +0530

    Fix webhook handler for all topics

--- app/routes/webhooks.jsx
+++ app/routes/webhooks.jsx
@@ -4,15 +4,15 @@ export const action = async ({ request }) => {
   const { topic } = await authenticate.webhook(request);
 
   switch (topic) {
+    case "APP_UNINSTALLED":
+      return new Response(null, { status: 200 });
+    case "APP_SCOPES_UPDATE":
+      return new Response(null, { status: 200 });
     case "CUSTOMERS_DATA_REQUEST":
       return new Response(null, { status: 200 });
     case "CUSTOMERS_REDACT":
       return new Response(null, { status: 200 });
     case "SHOP_REDACT":
-      return new Response(null, { status: 200 });
-    case "APP_UNINSTALLED":
-      return new Response(null, { status: 200 });
-    case "APP_SCOPES_UPDATE":
       return new Response(null, { status: 200 });
     default:
       return new Response("Unhandled webhook topic", { status: 404 });
